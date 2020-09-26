e0150fe1df5ccbf1cf3b0662de60e6f195b18887
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2020-09-26 09:53:28 +0300

    Add error 404 layout and minimal

--- application/routes/web.php
+++ application/routes/web.php
@@ -21,8 +21,8 @@
 Route::get('/','WelcomeController@index')->name('public.home.index');
 
 Route::set('404_override', function(){
-	show_404();
-//	ci()->blade->view('errors.404');
+//	show_404();
+	ci()->blade->view('errors.404');
 });
 
 Route::set('translate_uri_dashes',FALSE);
